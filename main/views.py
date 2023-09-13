8c06f4ddb3d38cff775bca25eca8f557952fb39d
syauqiarman <syauqi.armanaya@ui.ac.id>
2023-09-13 09:35:33 +0700

    Update views.py and main.html

--- main/views.py
+++ main/views.py
@@ -3,8 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'name': 'Syauqi Armanaya Syaki',
-        'class': 'PBP D'
+        'name' : 'Kaos hitam polos',
+        'amount': 20,
+        'description':'Kaos polos pria berbahan Cotton Combed 30s round neck reguler fit',
+        'price': 50000,
+        'category': 'fashion pria'
+        
     }
 
     return render(request, "main.html", context)
